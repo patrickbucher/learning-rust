9308fae597aa5268c04c0658b98fdd2579585d2e
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-07-19 08:50:19 +0200

    removed old code examples; finished notes on generics

--- code/points/src/main.rs
+++ code/points/src/main.rs
@@ -3,11 +3,18 @@ struct Point<T, U> {
     y: U,
 }
 
-fn main() {
-    let a = Point { x: 12, y: 7 };
-    let b = Point { x: 3.75, y: 2.12 };
-    println!("a=({}, {}), b=({}, {})", a.x, a.y, b.x, b.y);
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,  // T
+            y: other.y, // W
+        }
+    }
+}
 
-    let c = Point { x: 10, y: 3.14 };
-    println!("c=({}, {})", c.x, c.y);
+fn main() {
+    let a = Point { x: 1, y: 2.5 };
+    let b = Point { x: 3.99, y: 8 };
+    let p = a.mixup(b);
+    println!("p=({},{})", p.x, p.y);
 }
