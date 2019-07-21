f66686e8963c56095c116adbc9d15ba44b4dbcdd
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-07-21 17:45:28 +0200

    implemented function and test to reverse text

--- code/words/src/lib.rs
+++ code/words/src/lib.rs
@@ -1,5 +1,22 @@
-pub fn split_words(text: &str) -> Vec<&str> {
-    let mut words = vec![];
+pub fn reverse_words(text: &str) -> String {
+    let mut reversed_text = String::new();
+    let mut reversed: Vec<&str> = Vec::new();
+    let words = split_words(text);
+    for i in 0..words.len() {
+        let j = words.len() - 1 - i;
+        reversed.push(words[j])
+    }
+    for word in reversed {
+        if reversed_text != "" {
+            reversed_text.push(' ');
+        }
+        reversed_text.push_str(&word);
+    }
+    reversed_text
+}
+
+fn split_words(text: &str) -> Vec<&str> {
+    let mut words = Vec::new();
     let mut i = 0;
     let mut in_word = true;
     let bytes = text.as_bytes();
@@ -23,9 +40,16 @@ mod test {
     use super::*;
 
     #[test]
+    fn test_reverse() {
+        let text = "This is some test";
+        let reversed = reverse_words(text);
+        assert_eq!("test some is This", reversed);
+    }
+
+    #[test]
     fn test_split() {
-        let text = "This  is some    test .";
+        let text = "This is some test";
         let words = split_words(text);
-        assert_eq!(vec!["This", "is", "some", "test", "."], words);
+        assert_eq!(vec!["This", "is", "some", "test"], words);
     }
 }
