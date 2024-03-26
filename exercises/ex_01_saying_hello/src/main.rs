df522294ab7e4582e7441873bab1a9b69a27b09b
Patrick Bucher <patrick.bucher@mailbox.org>
2024-03-26 08:52:50 +0100

    exercise 1

--- exercises/ex_01_saying_hello/src/main.rs
+++ exercises/ex_01_saying_hello/src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name?");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("enter your name");
+    let name = input.trim();
+    println!("Hello, {name}, nice to meet you!");
+    // TODO: separate string concatenation and output (constraint)
 }
