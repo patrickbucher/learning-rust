490b11b0d292ae7465314a40f042abc96a44be85
Patrick Bucher <patrick.bucher@mailbox.org>
2024-03-31 13:46:26 +0200

    finished reading chapter 5

--- examples/ch05/src/main.rs
+++ examples/ch05/src/main.rs
@@ -12,6 +12,12 @@ struct Book {
 #[derive(Debug)]
 struct RGB(u8, u8, u8);
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     let rust_book = Book {
         title: "The Rust Programming Language".to_string(),
@@ -22,15 +28,52 @@ fn main() {
         edition: 2,
         price: 49.99,
     };
-    let first_edition = Book {
+    let mut first_edition = Book {
         edition: 1,
         year: 2017,
         pages: 499,
         ..rust_book
     };
+    first_edition.pages += 3;
+    println!("{}", dbg!(first_edition).quote());
+    println!(
+        "{}",
+        Book::new(
+            "Fooled by Randomness".to_string(),
+            "Nassim Taleb".to_string()
+        )
+        .quote()
+    );
+
     let red = RGB(0xff, 0, 0);
     println!("{:?}", red);
-    println!("{}", first_edition.quote());
+    println!("{}", red.code());
+
+    let r1 = Rectangle {
+        width: 4,
+        height: 3,
+    };
+    let r2 = r1.scale(2);
+    println!("{:?} {:?}", r1, r2);
+    println!("{:?} {:?}", r1.area(), r2.area());
+    println!("can {:?} hold {:?}? {}", r1, r2, r1.can_hold(&r2));
+    println!("can {:?} hold {:?}? {}", r2, r1, r2.can_hold(&r1));
+    println!("{:?}", Rectangle::new(7, 8));
+    println!("{:?}", Rectangle::square(5));
+}
+
+impl Book {
+    fn new(title: String, author: String) -> Self {
+        Book {
+            title,
+            author,
+            publisher: "".to_string(),
+            pages: 0,
+            year: 2024,
+            edition: 1,
+            price: 0.0,
+        }
+    }
 }
 
 impl Book {
@@ -39,5 +82,35 @@ impl Book {
             "{}: {}. {} ({}), {}p.",
             self.author, self.title, self.publisher, self.year, self.pages
         )
+    }
+}
+
+impl RGB {
+    fn code(&self) -> String {
+        format!("#{:02x}{:02x}{:02x}", self.0, self.1, self.2)
+    }
+}
+
+impl Rectangle {
+    fn scale(&self, factor: u32) -> Rectangle {
+        Rectangle {
+            width: self.width * factor,
+            height: self.height * factor,
+        }
+    }
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    fn new(width: u32, height: u32) -> Self {
+        Rectangle { width, height }
+    }
+    fn square(side: u32) -> Self {
+        Rectangle {
+            width: side,
+            height: side,
+        }
     }
 }
