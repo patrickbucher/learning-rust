1f5c609109e108ff6236938d21404b0a6d3f719d
Patrick Bucher <patrick.bucher@mailbox.org>
2024-04-14 10:11:38 +0200

    deref examples

--- examples/ch15/src/lib.rs
+++ examples/ch15/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::{Deref, DerefMut};
+
 enum Tree<T> {
     Node(Box<Tree<T>>, T, Box<Tree<T>>),
     Leaf(T),
@@ -9,6 +11,55 @@ fn build_tree() -> Tree<i32> {
         2,
         Box::new(Tree::Leaf::<i32>(3)),
     )
+}
+
+struct VerboseBox<T> {
+    value: T,
+}
+
+impl<T> VerboseBox<T> {
+    fn new(value: T) -> VerboseBox<T> {
+        VerboseBox { value }
+    }
+}
+
+impl<T> Deref for VerboseBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        println!("dereferencing");
+        &self.value
+    }
+}
+
+struct CounterBox<T> {
+    value: T,
+    writes: usize,
+}
+
+impl<T> CounterBox<T> {
+    fn new(value: T) -> CounterBox<T> {
+        CounterBox { value, writes: 0 }
+    }
+
+    fn write_count(&self) -> usize {
+        self.writes
+    }
+}
+
+impl<T> Deref for CounterBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+impl<T> DerefMut for CounterBox<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.writes += 1;
+        &mut self.value
+    }
 }
 
 #[cfg(test)]
@@ -33,5 +84,37 @@ mod tests {
         } else {
             panic!("tree is not a node");
         }
+    }
+
+    #[test]
+    fn follow_reference() {
+        let a = 4;
+        let b = &a;
+        let c = Box::new(a);
+
+        assert_eq!(a, 4);
+        assert_eq!(*b, 4);
+        assert_eq!(*c, 4);
+    }
+
+    #[test]
+    fn deref_verbose_box() {
+        let a = 3;
+        let b = &a;
+        let c = Box::new(a);
+        let d = VerboseBox::new(a);
+
+        assert_eq!(a, 3);
+        assert_eq!(*b, 3);
+        assert_eq!(*c, 3);
+        assert_eq!(*d, 3);
+    }
+
+    #[test]
+    fn deref_counter_box() {
+        let mut a = CounterBox::new(13);
+        *a += 1;
+        *a += 1;
+        assert_eq!(a.write_count(), 2);
     }
 }
