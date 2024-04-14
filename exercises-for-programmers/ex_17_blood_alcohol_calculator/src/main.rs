bd4c5369d8b8358578f653c1c13445f500571983
Patrick Bucher <patrick.bucher@mailbox.org>
2024-04-14 11:27:15 +0200

    fixed computation

--- exercises-for-programmers/ex_17_blood_alcohol_calculator/src/main.rs
+++ exercises-for-programmers/ex_17_blood_alcohol_calculator/src/main.rs
@@ -3,10 +3,10 @@ use akshually::prompt_line;
 fn main() {
     let weight: f32 = prompt_line("Weight (lbs): ").expect("weight");
     let gender: char = prompt_line("Gender [m/w]: ").expect("gender");
-    let drinks: u8 = prompt_line("Number of Drinks: ").expect("drinks");
+    let drinks: f32 = prompt_line("Number of Drinks: ").expect("drinks");
     let size: f32 = prompt_line("Size Drinks (oz): ").expect("size");
     let alc_vol: f32 = prompt_line("Alcohol Vol. %: ").expect("alc_vol");
-    let hours: u8 = prompt_line("Hours since last drink: ").expect("hours");
+    let hours: f32 = prompt_line("Hours since last drink: ").expect("hours");
 
     let (bac, legal) = blood_alcohol(weight, gender, drinks, size, alc_vol, hours);
 
@@ -21,14 +21,15 @@ fn main() {
 fn blood_alcohol(
     weight: f32,
     gender: char,
-    drinks: u8,
+    drinks: f32,
     size: f32,
     alc_vol: f32,
-    hours: u8,
+    hours: f32,
 ) -> (f32, bool) {
-    let alcohol_consumed = drinks as f32 * size * (alc_vol / 100.0);
+    let alcohol_consumed = drinks * size * (alc_vol / 100.0);
+    println!("alcohol consumed (oz): {}", alcohol_consumed);
     let ratio: f32 = if gender == 'm' { 0.73 } else { 0.66 };
-    let bac = alcohol_consumed * 5.14 / weight * ratio - 0.15 * hours as f32;
+    let bac = (alcohol_consumed * 5.14 / weight * ratio) - 0.015 * hours;
     let legal = bac < 0.08;
     if bac < 0.0 {
         (0.0, legal)
