36239e29082209c30664dfdb72ec54e26fc7f7ec
Patrick Bucher <patrick.bucher@mailbox.org>
2024-05-13 14:05:38 +0200

    properly dealing with all command line arguments

--- examples/soccer_table/src/main.rs
+++ examples/soccer_table/src/main.rs
@@ -7,11 +7,22 @@ use soccer_table::compute_table;
 fn main() {
     let mut args = env::args();
     if args.len() < 2 {
-        eprintln!("usage: soccer-table FOLDER [DAY]");
+        eprintln!("usage: soccer-table DIR [DAY]");
         process::exit(1);
     }
+
     let _bin = args.next().unwrap();
     let dir = args.next().expect("missing DIR argument");
-    let day = args.next().and_then(|day| day.parse::<usize>().ok()); // TODO: malformed -> fail!
+    let day = match args.next() {
+        Some(day) => match day.parse::<usize>() {
+            Ok(day) => Some(day),
+            Err(e) => {
+                eprintln!("parsing day '{day}': {e}");
+                process::exit(1);
+            }
+        },
+        None => None,
+    };
+
     let _table = compute_table(Path::new(&dir), day);
 }
