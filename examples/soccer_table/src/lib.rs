70aa2809788d155159dfde932bc45971754767a9
Patrick Bucher <patrick.bucher@mailbox.org>
2024-05-18 07:50:40 +0200

    restructured code; group single day table rows by team

--- examples/soccer_table/src/lib.rs
+++ examples/soccer_table/src/lib.rs
@@ -1,67 +1,10 @@
 mod parsing;
+mod table_row;
 
 use parsing::{list_relevant_files, read_lines, MatchResult};
-use std::cmp::Ordering;
 use std::path::Path;
-
-#[derive(Debug, PartialEq)]
-pub struct TableRow {
-    pub rank: u8,
-    pub team: String,
-    pub points: u8,
-    pub wins: u8,
-    pub defeats: u8,
-    pub ties: u8,
-    pub goals_scored: u8,
-    pub goals_conceded: u8,
-    pub goals_diff: i8,
-}
-
-impl TableRow {
-    fn from(result: MatchResult) -> (TableRow, TableRow) {
-        let mut home = TableRow {
-            rank: 0,
-            team: result.home_team,
-            points: 0,
-            wins: 0,
-            defeats: 0,
-            ties: 0,
-            goals_scored: result.home_goals,
-            goals_conceded: result.away_goals,
-            goals_diff: result.home_goals as i8 - result.away_goals as i8,
-        };
-        let mut away = TableRow {
-            rank: 0,
-            team: result.away_team,
-            points: 0,
-            wins: 0,
-            defeats: 0,
-            ties: 0,
-            goals_scored: result.away_goals,
-            goals_conceded: result.home_goals,
-            goals_diff: result.away_goals as i8 - result.home_goals as i8,
-        };
-        match result.home_goals.cmp(&result.away_goals) {
-            Ordering::Less => {
-                home.defeats = 1;
-                away.wins = 1;
-                away.points = 3;
-            }
-            Ordering::Greater => {
-                home.wins = 1;
-                away.defeats = 1;
-                home.points = 3;
-            }
-            Ordering::Equal => {
-                home.ties = 1;
-                away.ties = 1;
-                home.points = 1;
-                away.points = 1;
-            }
-        }
-        (home, away)
-    }
-}
+use table_row::TableRow;
+use std::collections::{HashMap, hash_map::Entry};
 
 pub fn compute_table(dir: &Path, day: Option<usize>) -> Result<Box<Vec<String>>, ()> {
     let mut lines: Vec<String> = Vec::new();
@@ -70,122 +13,37 @@ pub fn compute_table(dir: &Path, day: Option<usize>) -> Result<Box<Vec<String>>,
             lines.append(&mut read_lines(&p))
         }
     }
+    let mut single_rows: Vec<TableRow> = Vec::new();
     for line in lines {
         let r: Result<MatchResult, _> = line.try_into();
         match r {
-            Ok(m) => println!("{:?}", m),
+            Ok(m) => {
+                let (home, away) = TableRow::from(m);
+                single_rows.push(home);
+                single_rows.push(away);
+            }
             Err(e) => eprintln!("{}", e),
+        }
+    }
+    let grouped = group_by_team(single_rows);
+    for (k, v) in grouped {
+        println!("{k}:");
+        for row in v {
+            println!("\t{:?}", row);
         }
     }
     Ok(Box::new(Vec::new()))
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn rows_from_home_win() {
-        let result = MatchResult {
-            home_team: String::from("FC Rustaceans"),
-            away_team: String::from("COBOL FC 1958"),
-            home_goals: 3,
-            away_goals: 2,
-        };
-        let expected_home = TableRow {
-            rank: 0,
-            team: String::from("FC Rustaceans"),
-            points: 3,
-            wins: 1,
-            defeats: 0,
-            ties: 0,
-            goals_scored: 3,
-            goals_conceded: 2,
-            goals_diff: 1,
-        };
-        let expected_away = TableRow {
-            rank: 0,
-            team: String::from("COBOL FC 1958"),
-            points: 0,
-            wins: 0,
-            defeats: 1,
-            ties: 0,
-            goals_scored: 2,
-            goals_conceded: 3,
-            goals_diff: -1,
-        };
-        let (actual_home, actual_away) = TableRow::from(result);
-        assert_eq!(actual_home, expected_home);
-        assert_eq!(actual_away, expected_away);
+fn group_by_team(rows: Vec<TableRow>) -> HashMap<String, Vec<TableRow>> {
+    let mut rows_by_team: HashMap<String, Vec<TableRow>> = HashMap::new();
+    for row in rows {
+        match rows_by_team.entry(row.team.clone()) {
+            Entry::Occupied(mut e) => {e.get_mut().push(row.clone());},
+            Entry::Vacant(e) => {e.insert(vec![row.clone()]);},
+        }
     }
-
-    #[test]
-    fn rows_from_away_win() {
-        let result = MatchResult {
-            home_team: String::from("FC Rustaceans"),
-            away_team: String::from("COBOL FC 1958"),
-            home_goals: 2,
-            away_goals: 4,
-        };
-        let expected_home = TableRow {
-            rank: 0,
-            team: String::from("FC Rustaceans"),
-            points: 0,
-            wins: 0,
-            defeats: 1,
-            ties: 0,
-            goals_scored: 2,
-            goals_conceded: 4,
-            goals_diff: -2,
-        };
-        let expected_away = TableRow {
-            rank: 0,
-            team: String::from("COBOL FC 1958"),
-            points: 3,
-            wins: 1,
-            defeats: 0,
-            ties: 0,
-            goals_scored: 4,
-            goals_conceded: 2,
-            goals_diff: 2,
-        };
-        let (actual_home, actual_away) = TableRow::from(result);
-        assert_eq!(actual_home, expected_home);
-        assert_eq!(actual_away, expected_away);
-    }
-
-    #[test]
-    fn rows_from_tie() {
-        let result = MatchResult {
-            home_team: String::from("FC Rustaceans"),
-            away_team: String::from("COBOL FC 1958"),
-            home_goals: 3,
-            away_goals: 3,
-        };
-        let expected_home = TableRow {
-            rank: 0,
-            team: String::from("FC Rustaceans"),
-            points: 1,
-            wins: 0,
-            defeats: 0,
-            ties: 1,
-            goals_scored: 3,
-            goals_conceded: 3,
-            goals_diff: 0,
-        };
-        let expected_away = TableRow {
-            rank: 0,
-            team: String::from("COBOL FC 1958"),
-            points: 1,
-            wins: 0,
-            defeats: 0,
-            ties: 1,
-            goals_scored: 3,
-            goals_conceded: 3,
-            goals_diff: 0,
-        };
-        let (actual_home, actual_away) = TableRow::from(result);
-        assert_eq!(actual_home, expected_home);
-        assert_eq!(actual_away, expected_away);
-    }
+    rows_by_team
 }
+
+// TODO: test group_by_team
---
+++ examples/soccer_table/src/table_row.rs
@@ -0,0 +1,259 @@
+use crate::parsing::MatchResult;
+use std::cmp::Ordering;
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct TableRow {
+    pub rank: u8,
+    pub team: String,
+    pub points: u8,
+    pub wins: u8,
+    pub defeats: u8,
+    pub ties: u8,
+    pub goals_scored: u8,
+    pub goals_conceded: u8,
+    pub goals_diff: i8,
+}
+
+impl TableRow {
+    pub fn from(result: MatchResult) -> (Self, Self) {
+        let mut home = TableRow {
+            rank: 0,
+            team: result.home_team,
+            points: 0,
+            wins: 0,
+            defeats: 0,
+            ties: 0,
+            goals_scored: result.home_goals,
+            goals_conceded: result.away_goals,
+            goals_diff: result.home_goals as i8 - result.away_goals as i8,
+        };
+        let mut away = TableRow {
+            rank: 0,
+            team: result.away_team,
+            points: 0,
+            wins: 0,
+            defeats: 0,
+            ties: 0,
+            goals_scored: result.away_goals,
+            goals_conceded: result.home_goals,
+            goals_diff: result.away_goals as i8 - result.home_goals as i8,
+        };
+        match result.home_goals.cmp(&result.away_goals) {
+            Ordering::Less => {
+                home.defeats = 1;
+                away.wins = 1;
+                away.points = 3;
+            }
+            Ordering::Greater => {
+                home.wins = 1;
+                away.defeats = 1;
+                home.points = 3;
+            }
+            Ordering::Equal => {
+                home.ties = 1;
+                away.ties = 1;
+                home.points = 1;
+                away.points = 1;
+            }
+        }
+        (home, away)
+    }
+
+    pub fn combine(self, other: Self) -> Result<Self, String> {
+        if self.team != other.team {
+            Err(format!(
+                "cannot combine rows of teams '{}' and '{}'",
+                self.team, other.team
+            ))
+        } else {
+            Ok(TableRow {
+                rank: 0,
+                team: self.team.clone(),
+                points: self.points + other.points,
+                wins: self.wins + other.wins,
+                defeats: self.defeats + other.defeats,
+                ties: self.ties + other.ties,
+                goals_scored: self.goals_scored + other.goals_scored,
+                goals_conceded: self.goals_conceded + other.goals_conceded,
+                goals_diff: self.goals_diff + other.goals_diff,
+            })
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rows_from_home_win() {
+        let result = MatchResult {
+            home_team: String::from("FC Rustaceans"),
+            away_team: String::from("COBOL FC 1958"),
+            home_goals: 3,
+            away_goals: 2,
+        };
+        let expected_home = TableRow {
+            rank: 0,
+            team: String::from("FC Rustaceans"),
+            points: 3,
+            wins: 1,
+            defeats: 0,
+            ties: 0,
+            goals_scored: 3,
+            goals_conceded: 2,
+            goals_diff: 1,
+        };
+        let expected_away = TableRow {
+            rank: 0,
+            team: String::from("COBOL FC 1958"),
+            points: 0,
+            wins: 0,
+            defeats: 1,
+            ties: 0,
+            goals_scored: 2,
+            goals_conceded: 3,
+            goals_diff: -1,
+        };
+        let (actual_home, actual_away) = TableRow::from(result);
+        assert_eq!(actual_home, expected_home);
+        assert_eq!(actual_away, expected_away);
+    }
+
+    #[test]
+    fn rows_from_away_win() {
+        let result = MatchResult {
+            home_team: String::from("FC Rustaceans"),
+            away_team: String::from("COBOL FC 1958"),
+            home_goals: 2,
+            away_goals: 4,
+        };
+        let expected_home = TableRow {
+            rank: 0,
+            team: String::from("FC Rustaceans"),
+            points: 0,
+            wins: 0,
+            defeats: 1,
+            ties: 0,
+            goals_scored: 2,
+            goals_conceded: 4,
+            goals_diff: -2,
+        };
+        let expected_away = TableRow {
+            rank: 0,
+            team: String::from("COBOL FC 1958"),
+            points: 3,
+            wins: 1,
+            defeats: 0,
+            ties: 0,
+            goals_scored: 4,
+            goals_conceded: 2,
+            goals_diff: 2,
+        };
+        let (actual_home, actual_away) = TableRow::from(result);
+        assert_eq!(actual_home, expected_home);
+        assert_eq!(actual_away, expected_away);
+    }
+
+    #[test]
+    fn rows_from_tie() {
+        let result = MatchResult {
+            home_team: String::from("FC Rustaceans"),
+            away_team: String::from("COBOL FC 1958"),
+            home_goals: 3,
+            away_goals: 3,
+        };
+        let expected_home = TableRow {
+            rank: 0,
+            team: String::from("FC Rustaceans"),
+            points: 1,
+            wins: 0,
+            defeats: 0,
+            ties: 1,
+            goals_scored: 3,
+            goals_conceded: 3,
+            goals_diff: 0,
+        };
+        let expected_away = TableRow {
+            rank: 0,
+            team: String::from("COBOL FC 1958"),
+            points: 1,
+            wins: 0,
+            defeats: 0,
+            ties: 1,
+            goals_scored: 3,
+            goals_conceded: 3,
+            goals_diff: 0,
+        };
+        let (actual_home, actual_away) = TableRow::from(result);
+        assert_eq!(actual_home, expected_home);
+        assert_eq!(actual_away, expected_away);
+    }
+
+    #[test]
+    fn combine_rows_of_different_teams() {
+        let team_a_first = TableRow {
+            rank: 0,
+            team: String::from("FC Rustaceans"),
+            points: 3,
+            wins: 1,
+            defeats: 0,
+            ties: 0,
+            goals_scored: 3,
+            goals_conceded: 2,
+            goals_diff: 1,
+        };
+        let team_b_first = TableRow {
+            rank: 0,
+            team: String::from("COBOL FC 1958"),
+            points: 0,
+            wins: 0,
+            defeats: 1,
+            ties: 0,
+            goals_scored: 2,
+            goals_conceded: 3,
+            goals_diff: -1,
+        };
+        let actual = team_a_first.combine(team_b_first);
+        assert!(actual.is_err());
+    }
+
+    #[test]
+    fn combine_rows_win_and_defeat() {
+        let team_a_first = TableRow {
+            rank: 0,
+            team: String::from("FC Rustaceans"),
+            points: 3,
+            wins: 1,
+            defeats: 0,
+            ties: 0,
+            goals_scored: 3,
+            goals_conceded: 2,
+            goals_diff: 1,
+        };
+        let team_a_second = TableRow {
+            rank: 0,
+            team: String::from("FC Rustaceans"),
+            points: 0,
+            wins: 0,
+            defeats: 1,
+            ties: 0,
+            goals_scored: 2,
+            goals_conceded: 3,
+            goals_diff: -1,
+        };
+        let expected = TableRow {
+            rank: 0,
+            team: String::from("FC Rustaceans"),
+            points: 3,
+            wins: 1,
+            defeats: 1,
+            ties: 0,
+            goals_scored: 5,
+            goals_conceded: 5,
+            goals_diff: 0,
+        };
+        let actual = team_a_first.combine(team_a_second);
+        assert_eq!(actual, Ok(expected));
+    }
+}
