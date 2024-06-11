f006e7482638e5acfc785159c6f4f2ab1ca18bd3
Patrick Bucher <patrick.bucher@mailbox.org>
2024-06-11 19:42:37 +0200

    exercise 49

--- exercises-for-programmers/ex_49_flickr_photo_search/src/main.rs
+++ exercises-for-programmers/ex_49_flickr_photo_search/src/main.rs
@@ -1,15 +1,29 @@
 use akshually::io::prompt_line;
 use reqwest::blocking;
 use serde::Deserialize;
+use std::fs::{create_dir, remove_dir_all, write};
+use std::path::PathBuf;
 
 #[derive(Deserialize, Debug)]
 struct Payload {
+    items: Vec<Item>,
     title: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Item {
+    title: String,
+    link: String,
+    media: Media,
+}
+
+#[derive(Deserialize, Debug)]
+struct Media {
+    m: String,
 }
 
 fn main() {
     let prompt: String = prompt_line("Enter subject for photos: ").unwrap();
-    println!("{prompt}");
     let query_string = format!("?format=json&tags={}&nojsoncallback=true", prompt);
     let url = format!(
         "https://www.flickr.com/services/feeds/photos_public.gne{}",
@@ -17,8 +31,44 @@ fn main() {
     );
     let response = blocking::get(url).unwrap().text().unwrap();
     let payload: Result<Payload, _> = serde_json::from_str(&response);
-    match payload {
-        Err(e) => eprintln!("{e}"),
-        Ok(p) => println!("{:?}", p),
+    let payload = match payload {
+        Err(e) => panic!("{e}"),
+        Ok(p) => p,
+    };
+    let images: &Vec<(usize, String)> = &payload
+        .items
+        .iter()
+        .map(|i| i.media.m.clone())
+        .enumerate()
+        .collect();
+
+    let mut base_dir = PathBuf::new();
+    base_dir.push(".");
+    base_dir.push(prompt);
+    if base_dir.exists() {
+        remove_dir_all(&base_dir).unwrap();
     }
+    if let Err(e) = create_dir(&base_dir) {
+        panic!("{e}");
+    }
+
+    let mut html = String::from("<!DOCTYPE html><head><title>");
+    html.push_str(&payload.title);
+    html.push_str("</title></head><body>");
+    for (i, url) in images {
+        let mut target = base_dir.clone();
+        let relative_name = format!("{i}.jpg");
+        target.push(&relative_name);
+        let payload = blocking::get(url).unwrap().bytes().unwrap();
+        write(target, payload).unwrap();
+        html.push_str(&format!(
+            "<span style='padding: 1em;'><img src={}></span>",
+            relative_name
+        ));
+    }
+    html.push_str("</body></html>");
+
+    let mut html_path = base_dir.clone();
+    html_path.push("index.html");
+    write(html_path, html).unwrap();
 }
