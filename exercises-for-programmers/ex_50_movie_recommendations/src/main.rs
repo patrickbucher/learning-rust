0ba06e591825bae54218241a7b5a8e9e5d1f6ef5
Patrick Bucher <patrick.bucher@mailbox.org>
2024-06-12 07:31:15 +0200

    [WIP] exercise 50: fetch search results

--- exercises-for-programmers/ex_50_movie_recommendations/src/main.rs
+++ exercises-for-programmers/ex_50_movie_recommendations/src/main.rs
@@ -1,3 +1,18 @@
+use akshually::io::prompt_line;
+use reqwest::blocking;
+use std::env;
+
+const TOKEN_VAR: &str = "OMDB_API_TOKEN";
+
 fn main() {
-    println!("Hello, world!");
+    let token = match env::var(TOKEN_VAR) {
+        Ok(token) => token,
+        Err(err) => panic!("access env var {}: {}", TOKEN_VAR, err),
+    };
+    let query: String = prompt_line("Enter the name of a movie: ").unwrap();
+    let response = blocking::get(format!(
+        "http://www.omdbapi.com/?apikey={}&s={}",
+        token, query
+    )).unwrap().text().unwrap();
+    println!("{:?}", response);
 }
