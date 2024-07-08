388bfedfdc960abedda5f93a7a1c3ed04b205985
Patrick Bucher <patrick.bucher@mailbox.org>
2024-07-08 22:41:02 +0200

    implemented breadth-first search

--- grokking-algorithms/src/breadth_first_search.rs
+++ grokking-algorithms/src/breadth_first_search.rs
@@ -21,6 +21,31 @@ impl DirectedGraph {
     pub fn get_relations(&self, from: &str) -> Option<Vec<String>> {
         self.graph.get(from).cloned()
     }
+
+    pub fn has_path(&self, from: &str, to: &str) -> bool {
+        let initial_neighbours = match self.graph.get(from) {
+            None => return false,
+            Some(vec) => vec,
+        };
+        let mut search_queue: VecDeque<String> = VecDeque::from(initial_neighbours.clone());
+        let mut searched: HashSet<String> = HashSet::new();
+        while !search_queue.is_empty() {
+            match search_queue.pop_back() {
+                None => return false,
+                Some(node) => {
+                    if !searched.contains(&node) {
+                        if node == to {
+                            return true;
+                        } else if let Some(neighbours) = self.graph.get(&node) {
+                            search_queue.extend(neighbours.clone());
+                            searched.insert(node);
+                        }
+                    }
+                }
+            }
+        }
+        false
+    }
 }
 
 #[cfg(test)]
@@ -47,5 +72,26 @@ mod tests {
         let expected = None;
         let actual = graph.get_relations("Waldo");
         assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn search_from_to() {
+        let mut graph = DirectedGraph::new();
+        graph.add_relation("Alice", "Bob");
+        graph.add_relation("Alice", "Charlene");
+        graph.add_relation("Bob", "Elvira");
+        graph.add_relation("Charlene", "Dan");
+        graph.add_relation("Dan", "Freddy");
+        graph.add_relation("Dan", "Elvira");
+        graph.add_relation("Elvira", "Waldo");
+        graph.add_relation("Freddy", "Gina");
+        graph.add_relation("Gina", "Waldo");
+
+        assert!(graph.has_path("Alice", "Bob")); // Alice -> Bob
+        assert!(graph.has_path("Alice", "Elvira")); // Alice -> Bob -> Elvira
+        assert!(graph.has_path("Alice", "Waldo")); // Alice -> Bob -> Elvira -> Waldo
+        assert!(!graph.has_path("Bob", "Alice"));
+        assert!(!graph.has_path("Bob", "Dan"));
+        assert!(!graph.has_path("Waldo", "Alice"));
     }
 }
