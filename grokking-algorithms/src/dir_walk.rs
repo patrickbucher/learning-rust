14c03f927048afc03cc2ec19062132170afca1fb
Patrick Bucher <patrick.bucher@mailbox.org>
2024-07-09 21:51:08 +0200

    improved dir walk using clippy advice

--- grokking-algorithms/src/dir_walk.rs
+++ grokking-algorithms/src/dir_walk.rs
@@ -1,10 +1,10 @@
 use std::collections::VecDeque;
-use std::fs::{self, read_dir, DirEntry};
-use std::path::PathBuf;
+use std::fs::read_dir;
+use std::path::{PathBuf,Path};
 
-pub fn list_files_breadth_first(path: &PathBuf) -> Result<Vec<PathBuf>, std::io::Error> {
+pub fn list_files_breadth_first(path: &Path) -> Result<Vec<PathBuf>, std::io::Error> {
     let mut result: Vec<PathBuf> = Vec::new();
-    let mut worklist: VecDeque<PathBuf> = VecDeque::from([path.clone()]);
+    let mut worklist: VecDeque<PathBuf> = VecDeque::from([path.to_path_buf().clone()]);
     while !worklist.is_empty() {
         match worklist.pop_back() {
             Some(next) => {
@@ -24,8 +24,19 @@ pub fn list_files_breadth_first(path: &PathBuf) -> Result<Vec<PathBuf>, std::io:
     Ok(result)
 }
 
-pub fn list_files_depth_first(path: &PathBuf) -> Vec<PathBuf> {
-    Vec::new()
+pub fn list_files_depth_first(path: &Path) -> Result<Vec<PathBuf>, std::io::Error> {
+    if path.is_dir() {
+        let entries = read_dir(path)?;
+        let mut result: Vec<PathBuf> = Vec::new();
+        for entry in entries {
+            let entry = entry?;
+            let sub_entries = list_files_depth_first(&entry.path())?;
+            result.extend(sub_entries);
+        }
+        Ok(result)
+    } else {
+        Ok(vec![path.to_path_buf().clone()])
+    }
 }
 
 #[cfg(test)]
@@ -41,6 +52,21 @@ mod tests {
         let path = scaffold(&nodes);
         let expected: Vec<&str> = vec!["foo/foo.txt", "foo/bar/bar.txt", "foo/bar/qux/qux.txt"];
         let actual = list_files_breadth_first(&path).unwrap();
+        for (i, path) in actual.iter().enumerate() {
+            let path = path.clone().into_os_string().into_string().unwrap();
+            assert!(path.contains(&expected[i]));
+        }
+        cleanup(&path);
+    }
+
+    #[test]
+    fn depth_first() {
+        let nodes = ["foo", "bar", "qux"];
+        let path = scaffold(&nodes);
+        let mut expected: Vec<&str> = vec!["foo/bar/bar.txt", "foo/bar/qux/qux.txt", "foo/foo.txt"];
+        let mut actual = list_files_depth_first(&path).unwrap();
+        expected.sort();
+        actual.sort();
         for (i, path) in actual.iter().enumerate() {
             let path = path.clone().into_os_string().into_string().unwrap();
             assert!(path.contains(&expected[i]));
