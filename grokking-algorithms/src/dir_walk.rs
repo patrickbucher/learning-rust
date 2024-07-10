d603a0fdee4860a067cb2edea19e59351110a85c
Patrick Bucher <patrick.bucher@mailbox.org>
2024-07-10 07:17:48 +0200

    refactored test cases

--- grokking-algorithms/src/dir_walk.rs
+++ grokking-algorithms/src/dir_walk.rs
@@ -7,13 +7,13 @@ pub fn list_files_breadth_first(path: &Path) -> Result<Vec<PathBuf>, Error> {
     let mut result: Vec<PathBuf> = Vec::new();
     let mut worklist: VecDeque<PathBuf> = VecDeque::from([path.to_path_buf().clone()]);
     while !worklist.is_empty() {
-        match worklist.pop_back() {
+        match worklist.pop_front() {
             Some(next) => {
                 if next.is_dir() {
                     let entries = read_dir(next.clone())?;
                     for entry in entries {
                         let entry = entry?;
-                        worklist.push_front(entry.path());
+                        worklist.push_back(entry.path());
                     }
                 } else {
                     result.push(next.clone());
@@ -25,7 +25,7 @@ pub fn list_files_breadth_first(path: &Path) -> Result<Vec<PathBuf>, Error> {
     Ok(result)
 }
 
-pub fn list_files_depth_first(path: &Path) -> Result<Vec<PathBuf>, std::io::Error> {
+pub fn list_files_depth_first(path: &Path) -> Result<Vec<PathBuf>, Error> {
     if path.is_dir() {
         let entries = read_dir(path)?;
         let mut result: Vec<PathBuf> = Vec::new();
@@ -49,61 +49,83 @@ mod tests {
     use std::path::PathBuf;
 
     #[test]
-    fn breadth_first() {
-        let nodes = ["foo", "bar", "qux"];
-        let path = scaffold(&nodes);
-        let expected: Vec<&str> = vec!["foo/foo.txt", "foo/bar/bar.txt", "foo/bar/qux/qux.txt"];
-        let actual = list_files_breadth_first(&path).unwrap();
+    #[ignore] // NOTE: only run with --test-threads 1
+    fn new_breadth_first() -> Result<(), Error> {
+        let path = create_scaffold(get_entry_hiera())?;
+        let expected = vec![
+            "abc/1.txt",
+            "abc/2.txt",
+            "abc/3.txt",
+            "abc/def/4.txt",
+            "abc/def/5.txt",
+            "abc/def/6.txt",
+            "abc/def/ghi/7.txt",
+            "abc/def/ghi/8.txt",
+            "abc/def/ghi/9.txt",
+        ];
+        let actual = list_files_breadth_first(&path)?;
+        let mut actual: Vec<String> = actual
+            .into_iter()
+            .map(|p| p.into_os_string().into_string().unwrap())
+            .collect();
+        actual.sort();
         for (i, path) in actual.iter().enumerate() {
-            let path = path.clone().into_os_string().into_string().unwrap();
-            assert!(path.contains(&expected[i]));
+            assert!(path.contains(expected[i]));
         }
-        cleanup(&path);
+        cleanup(&path)?;
+        Ok(())
     }
 
     #[test]
-    fn depth_first() {
-        let nodes = ["foo", "bar", "qux"];
-        let path = scaffold(&nodes);
-        let mut expected: Vec<&str> = vec!["foo/bar/bar.txt", "foo/bar/qux/qux.txt", "foo/foo.txt"];
-        let mut actual = list_files_depth_first(&path).unwrap();
-        expected.sort();
+    #[ignore] // NOTE: only run with --test-threads 1
+    fn new_depth_first() -> Result<(), Error> {
+        let path = create_scaffold(get_entry_hiera())?;
+        let expected = vec![
+            "abc/1.txt",
+            "abc/2.txt",
+            "abc/3.txt",
+            "abc/def/4.txt",
+            "abc/def/5.txt",
+            "abc/def/6.txt",
+            "abc/def/ghi/7.txt",
+            "abc/def/ghi/8.txt",
+            "abc/def/ghi/9.txt",
+        ];
+        let actual = list_files_depth_first(&path)?;
+        let mut actual: Vec<String> = actual
+            .into_iter()
+            .map(|p| p.into_os_string().into_string().unwrap())
+            .collect();
         actual.sort();
         for (i, path) in actual.iter().enumerate() {
-            let path = path.clone().into_os_string().into_string().unwrap();
-            assert!(path.contains(&expected[i]));
+            assert!(path.contains(expected[i]));
         }
-        cleanup(&path);
+        cleanup(&path)?;
+        Ok(())
     }
 
     fn create_scaffold(hiera: Entry) -> Result<PathBuf, Error> {
-        let mut workdir = temp_dir();
+        let workdir = temp_dir();
         let mut root: Option<PathBuf> = None;
         let mut worklist: VecDeque<(PathBuf, Entry)> = VecDeque::from([(workdir, hiera)]);
         while !worklist.is_empty() {
             match worklist.pop_front() {
                 Some((workdir, entry)) => match entry {
-                    Entry::Dir {
-                        name: name,
-                        content: children,
-                    } => {
+                    Entry::Dir { name, content } => {
                         let mut dir = workdir.clone();
                         dir.push(name);
                         create_dir(dir.clone())?;
                         if let None = root {
                             root = Some(dir.clone());
                         }
-                        for child in children {
+                        for child in content {
                             worklist.push_back((dir.clone(), child));
                         }
                     }
-                    Entry::File {
-                        name: name,
-                        content: data,
-                    } => {
+                    Entry::File { name, content } => {
                         let mut path = workdir.clone();
                         path.push(name);
-                        write(path, data);
+                        write(path, content)?;
                     }
                 },
                 None => break,
@@ -174,23 +196,7 @@ mod tests {
         }
     }
 
-    fn scaffold(nodes: &[&str]) -> PathBuf {
-        let mut dir = temp_dir();
-        let mut root: Option<PathBuf> = None;
-        for node in nodes {
-            dir.push(node);
-            if let None = root {
-                root = Some(dir.clone());
-            }
-            let mut file = dir.clone();
-            file.push(format!("{node}.txt"));
-            create_dir(dir.clone());
-            write(file, node);
-        }
-        root.unwrap()
-    }
-
-    fn cleanup(dir: &PathBuf) {
-        let _ = remove_dir_all(dir);
+    fn cleanup(dir: &PathBuf) -> Result<(), Error> {
+        remove_dir_all(dir)
     }
 }
