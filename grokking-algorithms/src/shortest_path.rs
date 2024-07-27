9cc0d1609bdb183ce7a89f2cb9ea10c6f0f6e256
Patrick Bucher <patrick.bucher@mailbox.org>
2024-07-27 10:02:09 +0200

    started implementing shortest path algorithm: find cheapest outnode

--- grokking-algorithms/src/shortest_path.rs
+++ grokking-algorithms/src/shortest_path.rs
@@ -26,11 +26,38 @@ impl Graph {
     }
 
     pub fn get_shortest_path(&self, from: &str, to: &str) -> Result<Vec<String>, String> {
-        if !self.connections.contains_key(from.into()) {
-            return Err(format!("no such node '{from}' in graph"));
+        let outnodes = match self.connections.get(from.into()) {
+            Some(node) => node,
+            None => return Err(format!("no such node '{from}' in graph")),
+        };
+        let mut distances: HashMap<String, Option<usize>> = HashMap::new();
+        for (from, outnodes) in self.connections.clone() {
+            distances.insert(from.into(), None);
+            for outnode in outnodes.keys() {
+                distances.insert(outnode.into(), None);
+            }
         }
+        // TODO: use get_cheapest
+        // println!("{:?}", distances);
         Ok(Vec::new())
     }
+}
+
+fn get_cheapest(nodes: &HashMap<String, usize>) -> Option<(String, usize)> {
+    let mut cheapest: Option<(String, usize)> = None;
+    for (outnode, distance) in nodes {
+        cheapest = match cheapest {
+            Some((node, dist)) => {
+                if *distance < dist {
+                    Some((outnode.into(), *distance))
+                } else {
+                    Some((node.into(), dist))
+                }
+            }
+            None => Some((outnode.into(), *distance)),
+        }
+    }
+    cheapest
 }
 
 #[cfg(test)]
@@ -44,6 +71,19 @@ mod tests {
         graph.add("b", "c", 4);
         assert_eq!(graph.get_weight("a", "b"), Some(3));
         assert_eq!(graph.get_weight("b", "c"), Some(4));
+    }
+
+    #[test]
+    fn get_cheapest_node() {
+        let mut distances: HashMap<String, usize> = HashMap::new();
+        distances.insert("a".into(), 8);
+        distances.insert("b".into(), 3);
+        distances.insert("c".into(), 1);
+        distances.insert("d".into(), 5);
+        distances.insert("e".into(), 2);
+        let expected = Some(("c".into(), 1));
+        let actual = get_cheapest(&distances);
+        assert_eq!(actual, expected);
     }
 
     #[test]
