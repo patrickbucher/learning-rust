c5688e211936b989e4df8608cdab8c4450464384
Patrick Bucher <patrick.bucher@mailbox.org>
2024-08-03 08:16:22 +0200

    made subset code generic

--- grokking-algorithms/src/utils.rs
+++ grokking-algorithms/src/utils.rs
@@ -1,22 +1,22 @@
-fn create_subsets(items: &Vec<String>) -> Vec<Vec<String>> {
+pub fn create_subsets<T: Ord + Clone>(items: &[T]) -> Vec<Vec<T>> {
     let n: usize = items.len();
     let max = 2_u32.pow(n as u32);
-    let reversed_items: Vec<String> = items.iter().rev().cloned().collect();
-    let mut subsets: Vec<Vec<String>> = Vec::new();
+    let reversed_items: Vec<T> = items.iter().rev().cloned().collect();
+    let mut subsets: Vec<Vec<T>> = Vec::new();
     for i in 0..max {
-        let mut subset: Vec<String> = Vec::new();
+        let mut subset: Vec<T> = Vec::new();
         let mut mask = 1;
-        for j in 0..n {
+        for items in reversed_items.clone().into_iter().take(n) {
             if i & mask != 0 {
-                subset.push(reversed_items[j].clone());
+                subset.push(items);
             }
-            mask = mask << 1;
+            mask <<= 1;
         }
         subset.sort();
         subsets.push(subset);
     }
     subsets.sort();
-    subsets.sort_by(|l, r| l.len().cmp(&r.len()));
+    subsets.sort_by_key(|l| l.len());
     subsets
 }
 
@@ -52,6 +52,31 @@ mod tests {
                 .collect(),
         ];
         let actual: Vec<Vec<String>> = create_subsets(&items);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn subsets_of_numeric_set() {
+        let items: Vec<usize> = vec![0, 1, 2, 3];
+        let expected: Vec<Vec<usize>> = vec![
+            vec![],
+            vec![0],
+            vec![1],
+            vec![2],
+            vec![3],
+            vec![0, 1],
+            vec![0, 2],
+            vec![0, 3],
+            vec![1, 2],
+            vec![1, 3],
+            vec![2, 3],
+            vec![0, 1, 2],
+            vec![0, 1, 3],
+            vec![0, 2, 3],
+            vec![1, 2, 3],
+            vec![0, 1, 2, 3],
+        ];
+        let actual: Vec<Vec<usize>> = create_subsets(&items);
         assert_eq!(actual, expected);
     }
 }
