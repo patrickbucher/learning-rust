7a5e5a000db19abd6c136d48cfa100b33ede2064
Patrick Bucher <patrick.bucher@mailbox.org>
2024-08-17 08:28:50 +0200

    csgdsa chapter 6: insertion sort

---
+++ csgdsa/src/insertion_sort.rs
@@ -0,0 +1,54 @@
+pub fn insertion_sort<T: Clone + Ord>(values: &mut [T]) {
+    for i in 1..values.len() {
+        let current = values[i].clone();
+        let mut new_index: Option<usize> = None;
+        for j in (0..i).rev() {
+            if current < values[j] {
+                values[j + 1] = values[j].clone();
+                new_index = Some(j);
+            } else {
+                break;
+            }
+        }
+        if new_index.is_some() {
+            values[new_index.unwrap()] = current.clone();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn insertion_sort_empty() {
+        let mut values: Vec<usize> = Vec::new();
+        insertion_sort(&mut values);
+        let expected: Vec<usize> = Vec::new();
+        assert_eq!(values, expected);
+    }
+
+    #[test]
+    fn insertion_sort_sorted() {
+        let mut values: Vec<usize> = vec![1, 2, 3];
+        insertion_sort(&mut values);
+        let expected: Vec<usize> = vec![1, 2, 3];
+        assert_eq!(values, expected);
+    }
+
+    #[test]
+    fn insertion_sort_unsorted_small() {
+        let mut values: Vec<usize> = vec![7, 5, 6, 8, 4, 9, 3, 2, 0, 1];
+        insertion_sort(&mut values);
+        let expected: Vec<usize> = (0..10).collect();
+        assert_eq!(values, expected);
+    }
+
+    #[test]
+    fn insertion_sort_unsorted_big() {
+        let mut values: Vec<isize> = (-100..100).rev().collect();
+        insertion_sort(&mut values);
+        let expected: Vec<isize> = (-100..100).collect();
+        assert_eq!(values, expected);
+    }
+}
--- csgdsa/src/lib.rs
+++ csgdsa/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod bubble_sort;
+pub mod insertion_sort;
 pub mod misc;
 pub mod ordered_array;
 pub mod selection_sort;
