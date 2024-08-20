dcac6bdae8073f4dde2240734b14baebbe524fe0
Patrick Bucher <patrick.bucher@mailbox.org>
2024-08-20 08:00:30 +0200

    handle all possible cases

--- examples/sumtime/src/lib.rs
+++ examples/sumtime/src/lib.rs
@@ -14,8 +14,12 @@ impl TimeParser {
         match self.re.captures(time) {
             Some(matches) => {
                 if matches.len() == 3 {
-                    let (hours, minutes) = match (matches.get(1), matches.get(2)) {
+                    let hours = matches.get(1);
+                    let minutes = matches.get(2);
+                    let (hours, minutes) = match (hours, minutes) {
                         (Some(h), Some(m)) => (h.as_str(), m.as_str()),
+                        (Some(h), None) => (h.as_str(), "0"),
+                        (None, Some(m)) => ("0", m.as_str()),
                         _ => ("0", "0"),
                     };
                     (hours.parse().unwrap_or(0), minutes.parse().unwrap_or(0))
