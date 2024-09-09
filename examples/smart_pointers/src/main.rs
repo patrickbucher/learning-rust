7c658db26c88fb86e8927ba15cbe8a0c6a957279
Patrick Bucher <patrick.bucher@mailbox.org>
2024-09-09 12:23:50 +0200

    chapter 15 re-visited: deref and drop traits

--- examples/smart_pointers/src/main.rs
+++ examples/smart_pointers/src/main.rs
@@ -1,3 +1,7 @@
+use std::fmt::Debug;
+use std::mem::drop;
+use std::ops::Deref;
+
 #[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
@@ -24,6 +28,42 @@ fn boxes() {
     println!("{}", numbers.output());
 }
 
+struct SmartPointer<T: Debug> {
+    value: T,
+}
+
+impl<T: Debug> SmartPointer<T> {
+    pub fn new(value: T) -> Self {
+        SmartPointer { value }
+    }
+}
+
+impl<T: Debug> Deref for SmartPointer<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+impl<T: Debug> Drop for SmartPointer<T> {
+    fn drop(&mut self) {
+        eprintln!("{:?} just died", self.value);
+    }
+}
+
+fn deref_drop() {
+    let p = SmartPointer::new(13);
+    {
+        let q = SmartPointer::new(42);
+        let r = SmartPointer::new(99);
+        drop(r);
+        println!("q={}", *q);
+    }
+    println!("p={}", *p);
+}
+
 fn main() {
     boxes();
+    deref_drop();
 }
