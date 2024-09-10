ec365b45f251ab813ec99154f2ab82ab323096a8
Patrick Bucher <patrick.bucher@mailbox.org>
2024-09-10 07:24:06 +0200

    chapter 15 re-visited: Rc

--- examples/smart_pointers/src/main.rs
+++ examples/smart_pointers/src/main.rs
@@ -1,6 +1,7 @@
 use std::fmt::Debug;
 use std::mem::drop;
 use std::ops::Deref;
+use std::rc::Rc;
 
 #[derive(Debug)]
 enum List {
@@ -63,7 +64,60 @@ fn deref_drop() {
     println!("p={}", *p);
 }
 
+#[derive(Debug)]
+enum SharedList {
+    Cons(i32, Rc<SharedList>),
+    Nil,
+}
+
+impl SharedList {
+    fn output(&self) -> String {
+        match self {
+            SharedList::Cons(value, next) => format!("{value}, {}", next.output()),
+            SharedList::Nil => String::from("Nil"),
+        }
+    }
+}
+
+fn reference_counter() {
+    let tail = Box::new(List::Cons(2, Box::new(List::Cons(1, Box::new(List::Nil)))));
+    println!("{}", tail.output());
+
+    let head_increment = Box::new(List::Cons(3, tail));
+    println!("{}", head_increment.output());
+
+    // FIXME: value "tail" used here after move
+    // let head_doubled = Box::new(List::Cons(4, tail));
+    // println!("{}", head_doubled.output());
+
+    let tail = Rc::new(SharedList::Cons(
+        2,
+        Rc::new(SharedList::Cons(1, Rc::new(SharedList::Nil))),
+    ));
+    println!("{} (count: {})", tail.output(), Rc::strong_count(&tail));
+
+    {
+        let head_increment = SharedList::Cons(3, Rc::clone(&tail));
+        println!(
+            "{}, (count: {})",
+            head_increment.output(),
+            Rc::strong_count(&tail)
+        );
+        {
+            let head_doubled = SharedList::Cons(4, Rc::clone(&tail));
+            println!(
+                "{} (count: {})",
+                head_doubled.output(),
+                Rc::strong_count(&tail)
+            );
+        }
+        println!("count: {}", Rc::strong_count(&tail));
+    }
+    println!("count: {}", Rc::strong_count(&tail));
+}
+
 fn main() {
     boxes();
     deref_drop();
+    reference_counter();
 }
