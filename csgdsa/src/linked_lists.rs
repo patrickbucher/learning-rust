37926d154d1d4b2d4ebc42e2e6b165850e34fa1a
Patrick Bucher <patrick.bucher@mailbox.org>
2024-09-11 19:55:24 +0200

    WIP: linked list

--- csgdsa/src/linked_lists.rs
+++ csgdsa/src/linked_lists.rs
@@ -7,18 +7,35 @@ impl<T> Node<T>
 where
     T: Clone,
 {
-    fn new(value: T) -> Self {
+    pub fn new(value: T) -> Self {
         Node { value, next: None }
     }
 
-    fn prepend(self, value: T) -> Self {
+    pub fn prepend(self, value: T) -> Self {
         Node {
             value,
             next: Some(Box::new(self)),
         }
     }
 
-    fn get_values(&self) -> Vec<T> {
+    pub fn append(&mut self, value: T) {
+        let next = Some(Box::new(Node { value, next: None }));
+        if self.next.is_none() {
+            self.next = next;
+            return;
+        }
+        let mut temp = &mut self.next;
+        while let Some(node) = temp {
+            if node.next.is_some() {
+                temp = &mut node.next;
+            } else {
+                break;
+            }
+        }
+        // TODO: at this point, temp is the tail
+    }
+
+    pub fn get_values(&self) -> Vec<T> {
         let mut values: Vec<T> = vec![self.value.clone()];
         let mut temp = &self.next;
         while let Some(node) = temp {
@@ -33,11 +50,21 @@ where
 mod tests {
     use super::*;
 
-    fn append_items_sequentially() {
-        let mut head: Node<usize> = Node::new(4);
+    #[test]
+    fn test_prepend() {
+        let head: Node<usize> = Node::new(4);
         let head = head.prepend(3);
         let head = head.prepend(2);
         let head = head.prepend(1);
+        assert_eq!(head.get_values(), vec![1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn test_append() {
+        let mut head: Node<usize> = Node::new(1);
+        head.append(2);
+        head.append(3);
+        head.append(4);
         assert_eq!(head.get_values(), vec![1, 2, 3, 4]);
     }
 }
