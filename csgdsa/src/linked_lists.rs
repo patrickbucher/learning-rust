6038eafb73307e16f8111fe8ec9b2659aa4b6ea8
Patrick Bucher <patrick.bucher@mailbox.org>
2024-09-11 18:42:44 +0200

    simplified linked list: prepend, get_values

--- csgdsa/src/linked_lists.rs
+++ csgdsa/src/linked_lists.rs
@@ -1,31 +1,31 @@
-pub struct Node<T> {
+pub struct Node<T: Clone> {
     value: T,
     next: Option<Box<Node<T>>>,
 }
 
-pub struct LinkedList<T> {
-    head: Option<Box<Node<T>>>,
-}
-
-impl<T> LinkedList<T> {
-    fn new() -> Self {
-        LinkedList { head: None }
+impl<T> Node<T>
+where
+    T: Clone,
+{
+    fn new(value: T) -> Self {
+        Node { value, next: None }
     }
 
-    fn append(&mut self, value: T) {
-        let new = Node { value, next: None };
-        let head = match &self.head {
-            Some(node) => node,
-            None => {
-                self.head = Some(Box::new(new));
-                return;
-            }
-        };
-        let mut temp: &mut Box<Node<T>> = head;
-        while let Some(ref mut next) = &temp.next {
-            temp = &mut next;
+    fn prepend(self, value: T) -> Self {
+        Node {
+            value,
+            next: Some(Box::new(self)),
         }
-        temp.next = Some(Box::new(new));
+    }
+
+    fn get_values(&self) -> Vec<T> {
+        let mut values: Vec<T> = vec![self.value.clone()];
+        let mut temp = &self.next;
+        while let Some(node) = temp {
+            values.push(node.value.clone());
+            temp = &node.next;
+        }
+        values
     }
 }
 
@@ -34,6 +34,10 @@ mod tests {
     use super::*;
 
     fn append_items_sequentially() {
-        let mut list: LinkedList<usize> = LinkedList::new();
+        let mut head: Node<usize> = Node::new(4);
+        let head = head.prepend(3);
+        let head = head.prepend(2);
+        let head = head.prepend(1);
+        assert_eq!(head.get_values(), vec![1, 2, 3, 4]);
     }
 }
