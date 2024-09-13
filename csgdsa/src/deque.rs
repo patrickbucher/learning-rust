3dd4db89fa29f859a2b65d85d47e9876f23db8d5
Patrick Bucher <patrick.bucher@mailbox.org>
2024-09-13 06:25:00 +0200

    test case for deque

--- csgdsa/src/deque.rs
+++ csgdsa/src/deque.rs
@@ -12,11 +12,51 @@ pub struct Node<T: Clone> {
     prev: Option<Rc<RefCell<Node<T>>>>,
 }
 
-impl<T> Deque<T> where T: Clone {
-    pub fn enqueue(&mut self, value: T) {
+impl<T> Deque<T>
+where
+    T: Clone,
+{
+    pub fn new() -> Self {
+        Deque {
+            head: None,
+            tail: None,
+        }
     }
+
+    pub fn is_empty(&self) -> bool {
+        self.head.is_none()
+    }
+
+    pub fn enqueue(&mut self, value: T) {}
 
     pub fn dequeue(&mut self) -> Option<T> {
         None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    fn test_enqueue_dequeue() {
+        let mut deque: Deque<usize> = Deque::new();
+        assert!(deque.is_empty());
+
+        deque.enqueue(3);
+        assert!(!deque.is_empty());
+
+        assert_eq!(dequeue.dequeue(), Some(3));
+        assert!(deque.is_empty());
+        assert_eq!(dequeue.dequeue(), None);
+
+        deque.enqueue(7);
+        deque.enqueue(2);
+        deque.enqueue(5);
+        assert!(!deque.is_empty());
+
+        assert_eq!(deque.dequeue(), Some(7));
+        assert_eq!(deque.dequeue(), Some(2));
+        assert!(!deque.is_empty());
+        assert_eq!(deque.dequeue(), Some(5));
+        assert!(deque.is_empty());
+        assert_eq!(deque.dequeue(), None);
     }
 }
