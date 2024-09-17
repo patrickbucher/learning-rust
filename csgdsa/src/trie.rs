09934c2e54b5f08cb79e1273da2f3e634c1f4f7f
Patrick Bucher <patrick.bucher@mailbox.org>
2024-09-17 22:02:45 +0200

    trie: find by prefix

--- csgdsa/src/trie.rs
+++ csgdsa/src/trie.rs
@@ -19,6 +19,10 @@ impl Trie {
     pub fn insert(&mut self, word: &str) {
         self.root.insert(word);
     }
+
+    fn find_by_prefix(&self, prefix: &str) -> Option<Node> {
+        self.root.find_by_prefix(prefix)
+    }
 }
 
 impl Default for Trie {
@@ -66,6 +70,20 @@ impl Node {
                 Some(e)
             });
     }
+
+    fn find_by_prefix(&self, prefix: &str) -> Option<Node> {
+        let mut chars = prefix.chars();
+        match chars.next() {
+            Some(c) => match self.children.get(&c) {
+                Some(child) => match child {
+                    Some(node) => node.find_by_prefix(&String::from_iter(chars)),
+                    None => None,
+                },
+                None => None,
+            },
+            None => Some(self.clone()),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -105,4 +123,40 @@ mod tests {
         let bat_children = ba_children.get(&'t').unwrap().clone().unwrap().children;
         assert!(bat_children.contains_key(&EOW));
     }
+
+    #[test]
+    fn test_find_by_prefix() {
+        let mut words = Trie::new();
+        words.insert("car");
+        words.insert("cat");
+        words.insert("catapult");
+        words.insert("catnip");
+        words.insert("catwalk");
+        words.insert("cattle");
+        words.insert("cauldron");
+
+        let c_node = words.find_by_prefix("c").unwrap();
+        let children = c_node.children;
+        assert!(children.contains_key(&'a'));
+
+        let ca_node = words.find_by_prefix("ca").unwrap();
+        let children = ca_node.children;
+        assert!(children.contains_key(&'r'));
+        assert!(children.contains_key(&'t'));
+        assert!(children.contains_key(&'u'));
+
+        let car_node = words.find_by_prefix("car").unwrap();
+        let children = car_node.children;
+        assert!(children.contains_key(&EOW));
+
+        let cat_node = words.find_by_prefix("cat").unwrap();
+        let children = cat_node.children;
+        assert!(children.contains_key(&EOW));
+        assert!(children.contains_key(&'a'));
+        assert!(children.contains_key(&'n'));
+        assert!(children.contains_key(&'w'));
+        assert!(children.contains_key(&'t'));
+    }
+
+    // TODO: completions: prefix + collected suffixes from sub-nodes
 }
