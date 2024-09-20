e11489702daf914b69b4729fb03037f0d300a113
Patrick Bucher <patrick.bucher@mailbox.org>
2024-09-20 17:26:30 +0200

    graph: get vertex

--- csgdsa/src/graph.rs
+++ csgdsa/src/graph.rs
@@ -8,6 +8,7 @@ pub enum Kind {
     Undirected,
 }
 
+#[derive(Clone, Debug, PartialEq)]
 struct Vertex<K: Eq + Clone + Hash, V: Clone> {
     id: K,
     value: V,
@@ -49,6 +50,13 @@ where
         self.edges.insert(id.clone(), HashSet::new());
         Ok(())
     }
+
+    pub fn get_vertex(&self, id: K) -> Option<Vertex<K, V>> {
+        match self.vertices.get(&id) {
+            Some(node) => Some(node.clone()),
+            None => None,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -56,12 +64,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_add_vertex() {
+    fn test_add_get_vertex() {
         let mut graph = Graph::new(Kind::Directed);
         assert_eq!(graph.add_vertex("a", "Alice"), Ok(()));
         assert_eq!(graph.add_vertex("b", "Bob"), Ok(()));
-        assert_eq!(graph.add_vertex("c", "Charlene"), Ok(()));
-        assert_eq!(graph.add_vertex("d", "Dan"), Ok(()));
-        assert_eq!(graph.add_vertex("b", "Bernardo"), Err(GraphError::VertexAlreadyExists));
+        assert_eq!(
+            graph.add_vertex("b", "Bernardo"),
+            Err(GraphError::VertexAlreadyExists)
+        );
+
+        assert_eq!(
+            graph.get_vertex("a"),
+            Some(Vertex {
+                id: "a",
+                value: "Alice"
+            })
+        );
+        assert_eq!(
+            graph.get_vertex("b"),
+            Some(Vertex {
+                id: "b",
+                value: "Bob"
+            })
+        );
+        assert_eq!(graph.get_vertex("c"), None);
     }
 }
