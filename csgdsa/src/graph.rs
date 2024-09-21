26271bda80d7d19118ffe82d83bfd1ca98cbfb0b
Patrick Bucher <patrick.bucher@mailbox.org>
2024-09-21 07:23:05 +0200

    graph: support both weighted/unweighted edges

--- csgdsa/src/graph.rs
+++ csgdsa/src/graph.rs
@@ -2,15 +2,21 @@
 // - delete vertex (with all its edges)
 // - delete edge
 // - ???
+// weighted/unweighted edges
 
 use std::collections::HashMap;
-use std::collections::HashSet;
 use std::hash::Hash;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum Kind {
     Directed,
     Undirected,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum EdgeType {
+    Weighted(isize),
+    Unweighted,
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -21,8 +27,9 @@ pub struct Vertex<K: Eq + Clone + Hash, V: Clone> {
 
 pub struct Graph<K: Eq + Clone + Hash, V: Clone> {
     kind: Kind,
+    edge_type: EdgeType,
     vertices: HashMap<K, Vertex<K, V>>,
-    edges: HashMap<K, HashSet<K>>,
+    edges: HashMap<K, HashMap<K, EdgeType>>,
 }
 
 #[derive(PartialEq, Debug)]
@@ -30,6 +37,7 @@ pub enum GraphError {
     VertexAlreadyExists,
     VertexInexistant,
     EdgeAlreadyExists,
+    EdgeTypeMismatch,
 }
 
 impl<K, V> Graph<K, V>
@@ -37,9 +45,10 @@ where
     K: Eq + Clone + Hash,
     V: Clone,
 {
-    pub fn new(kind: Kind) -> Self {
+    pub fn new(kind: Kind, edge_type: EdgeType) -> Self {
         Graph {
             kind,
+            edge_type,
             vertices: HashMap::new(),
             edges: HashMap::new(),
         }
@@ -54,7 +63,7 @@ where
             value,
         };
         self.vertices.insert(id.clone(), vertex);
-        self.edges.insert(id.clone(), HashSet::new());
+        self.edges.insert(id.clone(), HashMap::new());
         Ok(())
     }
 
@@ -62,7 +71,21 @@ where
         self.vertices.get(&id).cloned()
     }
 
-    pub fn add_edge(&mut self, from: K, to: K) -> Result<(), GraphError> {
+    pub fn add_edge_weighted(&mut self, from: K, to: K, weight: isize) -> Result<(), GraphError> {
+        if let EdgeType::Unweighted = self.edge_type {
+            return Err(GraphError::EdgeTypeMismatch);
+        }
+        self.add_edge(from, to, EdgeType::Weighted(weight))
+    }
+
+    pub fn add_edge_unweighted(&mut self, from: K, to: K) -> Result<(), GraphError> {
+        if let EdgeType::Weighted(_) = self.edge_type {
+            return Err(GraphError::EdgeTypeMismatch);
+        }
+        self.add_edge(from, to, EdgeType::Unweighted)
+    }
+
+    fn add_edge(&mut self, from: K, to: K, edge_type: EdgeType) -> Result<(), GraphError> {
         let _ = self
             .get_vertex(from.clone())
             .ok_or(GraphError::VertexInexistant)?;
@@ -74,29 +97,29 @@ where
                 self.edges
                     .entry(from.clone())
                     .and_modify(|e| {
-                        e.insert(to.clone());
+                        e.insert(to.clone(), edge_type.clone());
                     })
-                    .or_insert(HashSet::from([to.clone()]));
+                    .or_insert(HashMap::from([(to.clone(), edge_type.clone())]));
             }
             Kind::Undirected => {
                 self.edges
                     .entry(from.clone())
                     .and_modify(|e| {
-                        e.insert(to.clone());
+                        e.insert(to.clone(), edge_type.clone());
                     })
-                    .or_insert(HashSet::from([to.clone()]));
+                    .or_insert(HashMap::from([(to.clone(), edge_type.clone())]));
                 self.edges
                     .entry(to.clone())
                     .and_modify(|e| {
-                        e.insert(from.clone());
+                        e.insert(from.clone(), edge_type.clone());
                     })
-                    .or_insert(HashSet::from([from.clone()]));
+                    .or_insert(HashMap::from([(from.clone(), edge_type.clone())]));
             }
         }
         Ok(())
     }
 
-    pub fn get_edges_to(&self, from: K) -> Result<HashSet<K>, GraphError> {
+    pub fn get_edges_to(&self, from: K) -> Result<HashMap<K, EdgeType>, GraphError> {
         self.edges
             .get(&from)
             .ok_or(GraphError::VertexInexistant)
@@ -110,7 +133,7 @@ mod tests {
 
     #[test]
     fn test_add_get_vertex() {
-        let mut graph = Graph::new(Kind::Directed);
+        let mut graph = Graph::new(Kind::Directed, EdgeType::Unweighted);
         assert_eq!(graph.add_vertex("a", "Alice"), Ok(()));
         assert_eq!(graph.add_vertex("b", "Bob"), Ok(()));
         assert_eq!(
@@ -136,41 +159,71 @@ mod tests {
     }
 
     #[test]
-    fn test_add_get_edge_directed() -> Result<(), GraphError> {
-        let mut graph = Graph::new(Kind::Directed);
+    fn test_add_get_edge_directed_unweighted() -> Result<(), GraphError> {
+        let mut graph = Graph::new(Kind::Directed, EdgeType::Unweighted);
         graph.add_vertex("a", "Anderson")?;
         graph.add_vertex("b", "Beavis")?;
         graph.add_vertex("c", "Carla")?;
         graph.add_vertex("d", "Daria")?;
-        graph.add_edge("a", "b")?;
-        graph.add_edge("a", "d")?;
-        graph.add_edge("b", "d")?;
-        graph.add_edge("d", "a")?;
+        graph.add_edge_unweighted("a", "b")?;
+        graph.add_edge_unweighted("a", "d")?;
+        graph.add_edge_unweighted("b", "d")?;
+        graph.add_edge_unweighted("d", "a")?;
 
-        assert_eq!(graph.get_edges_to("a"), Ok(HashSet::from(["b", "d"])));
-        assert_eq!(graph.get_edges_to("b"), Ok(HashSet::from(["d"])));
-        assert_eq!(graph.get_edges_to("c"), Ok(HashSet::new()));
-        assert_eq!(graph.get_edges_to("d"), Ok(HashSet::from(["a"])));
+        assert_eq!(
+            graph.get_edges_to("a"),
+            Ok(HashMap::from([
+                ("b", EdgeType::Unweighted),
+                ("d", EdgeType::Unweighted)
+            ]))
+        );
+        assert_eq!(
+            graph.get_edges_to("b"),
+            Ok(HashMap::from([("d", EdgeType::Unweighted)]))
+        );
+        assert_eq!(graph.get_edges_to("c"), Ok(HashMap::new()));
+        assert_eq!(
+            graph.get_edges_to("d"),
+            Ok(HashMap::from([("a", EdgeType::Unweighted)]))
+        );
         assert_eq!(graph.get_edges_to("e"), Err(GraphError::VertexInexistant));
         Ok(())
     }
 
     #[test]
-    fn test_add_get_edge_undirected() -> Result<(), GraphError> {
-        let mut graph = Graph::new(Kind::Undirected);
+    fn test_add_get_edge_undirected_unweighted() -> Result<(), GraphError> {
+        let mut graph = Graph::new(Kind::Undirected, EdgeType::Unweighted);
         graph.add_vertex("a", "Anderson")?;
         graph.add_vertex("b", "Beavis")?;
         graph.add_vertex("c", "Carla")?;
         graph.add_vertex("d", "Daria")?;
-        graph.add_edge("a", "b")?;
-        graph.add_edge("a", "d")?;
-        graph.add_edge("b", "d")?;
-        graph.add_edge("d", "a")?;
+        graph.add_edge_unweighted("a", "b")?;
+        graph.add_edge_unweighted("a", "d")?;
+        graph.add_edge_unweighted("b", "d")?;
+        graph.add_edge_unweighted("d", "a")?;
 
-        assert_eq!(graph.get_edges_to("a"), Ok(HashSet::from(["b", "d"])));
-        assert_eq!(graph.get_edges_to("b"), Ok(HashSet::from(["a", "d"])));
-        assert_eq!(graph.get_edges_to("c"), Ok(HashSet::new()));
-        assert_eq!(graph.get_edges_to("d"), Ok(HashSet::from(["a", "b"])));
+        assert_eq!(
+            graph.get_edges_to("a"),
+            Ok(HashMap::from([
+                ("b", EdgeType::Unweighted),
+                ("d", EdgeType::Unweighted)
+            ]))
+        );
+        assert_eq!(
+            graph.get_edges_to("b"),
+            Ok(HashMap::from([
+                ("a", EdgeType::Unweighted),
+                ("d", EdgeType::Unweighted)
+            ]))
+        );
+        assert_eq!(graph.get_edges_to("c"), Ok(HashMap::new()));
+        assert_eq!(
+            graph.get_edges_to("d"),
+            Ok(HashMap::from([
+                ("a", EdgeType::Unweighted),
+                ("b", EdgeType::Unweighted)
+            ]))
+        );
         assert_eq!(graph.get_edges_to("e"), Err(GraphError::VertexInexistant));
         Ok(())
     }
--- diary.md
+++ diary.md
@@ -1,8 +1,9 @@
 # Day 180 (2024-05-20)
 
-I started reading chapter 18 and started implementing a graph module. I won't
-bother with vertices connected to one another, but rather use hash maps, which
-will make the implementation in Rust much simpler.
+I started reading chapter 18 and implementing a graph module. I won't bother
+with vertices connected to one another, but rather use hash maps, which will
+make the implementation in Rust much simpler. In the evening, I implemented the
+basic operations to add vertices and edges (directed and undirected).
 
 # Day 179 (2024-05-19)
 
